67e4120ef26a577c61d99c2c88b301c91cb8d550
doodletaco <47832687+doodletaco@users.noreply.github.com>
2021-09-19 22:57:26 -0700

    Initialize things I think I will use.
    I added pygame, the library I will use and initialized the main game file with comments of what I think I need to do in order to get started.

---
+++ run_game.py
@@ -0,0 +1,15 @@
+###
+### Author: doodleTaco
+### Description: My entry to Pyweek 32.
+###              A card-matching game, centered around escaping a dream.
+###
+
+# create board
+
+# accept user input
+
+# check if matching
+
+# update board
+
+# game loop
